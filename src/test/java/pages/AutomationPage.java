1b7ca0863825a36e8ae6d1507944eb821e390285
Oner Tekin <tekinoner37@gmail.com>
2022-05-29 20:03:17 +0300

    29.05.2022

--- src/test/java/pages/AutomationPage.java
+++ src/test/java/pages/AutomationPage.java
@@ -12,14 +12,19 @@ public class AutomationPage {
 
     @FindBy(xpath="//html[@lang='en']")
     public WebElement homePage;
+
     @FindBy(xpath="//*[text()='View Cart']")
     public WebElement viewCart;
+
     @FindBy(id="cart_items")
     public WebElement cartPage;
+
     @FindBy(xpath="//tbody")
     public WebElement product;
+
     @FindBy(xpath="//*[text()='Category']")
     public WebElement category;
+
     @FindBy(xpath="(//span[@class='badge pull-right'])[1]")
     public WebElement womenCategory;
 
@@ -28,22 +33,51 @@ public class AutomationPage {
 
     @FindBy(xpath="(//span[@class='badge pull-right'])[3]")
     public WebElement kidsCategory;
+
     @FindBy(xpath="//*[text()='Dress ']")
     public WebElement dressWomenCategory;
+
     @FindBy(xpath="//div[@class='panel-body']//li[2]")
     public WebElement topsWomenCategory;
+
     @FindBy(xpath="//div[@class='panel-body']//li[3]")
     public WebElement sareeWomenCategory;
+
     @FindBy(xpath="//div[@class='left-sidebar']")
     public WebElement categoryPage;
+
     @FindBy(xpath="//*[text()='Women - Dress Products']")
     public WebElement womenDressProduct;
+
     @FindBy(xpath="//*[text()='Tshirts ']")
     public WebElement tshirtMenCategory;
+
     @FindBy(xpath="//div[@id='Men']//li[2]")
     public WebElement jeansMenCategory;
+
     @FindBy(xpath="//*[text()='Men - Tshirts Products']")
     public WebElement menTshirtProduct;
+
+    @FindBy(xpath="//*[text()=' Products']")
+    public WebElement productButton;
+
+    @FindBy(xpath="//div[@class='brands-name']")
+    public WebElement brands;
+
+    @FindBy(xpath="//*[text()='Polo']")
+    public WebElement polo;
+
+    @FindBy(xpath="//div[@class='features_items']")
+    public WebElement brandsPoloProducts;
+
+    @FindBy(xpath="//*[text()='H&M']")
+    public WebElement HM;
+
+    @FindBy(xpath="//div[@class='features_items']")
+    public WebElement brandsHMProducts;
+
+
+
 
 
 
---
+++ src/test/java/tests/Auto19.java
@@ -0,0 +1,39 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AutomationPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+
+public class Auto19 {
+    /*
+    1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Click on 'Products' button
+4. Verify that Brands are visible on left side bar
+5. Click on any brand name
+6. Verify that user is navigated to brand page and brand products are displayed
+7. On left side bar, click on any other brand link
+8. Verify that user is navigated to that brand page and can see products
+     */
+    @Test
+    public void test(){
+
+        AutomationPage automationPage = new AutomationPage();
+        Driver.getDriver().get(ConfigReader.getProperty("automationUrl"));
+        automationPage.productButton.click();
+        Assert.assertTrue(automationPage.brands.isDisplayed());
+        automationPage.polo.click();
+        Assert.assertTrue(automationPage.brandsPoloProducts.isDisplayed());
+        automationPage.HM.click();
+        Assert.assertTrue(automationPage.brandsHMProducts.isDisplayed());
+        Driver.closeDriver();
+
+
+
+
+
+    }
+}
